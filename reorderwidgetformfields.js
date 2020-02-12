d059d79cd6e3f7ea028c8f6021e5a6bb9cf10c1b
Kris-S <kristopher@wix.com>
2020-02-12 10:47:45 -0500

    Update reorderwidgetformfields.js

--- reorderwidgetformfields.js
+++ reorderwidgetformfields.js
@@ -1,20 +1,12 @@
-// DOM element we want to observe
 var targetNode = document.getElementsByClassName('forms-container')[0];
-// options for the observer
 var config = { childList: true };
-// callback will execute when mutations are observed
 var callback = function(mutationsList){ arrangeFields() };
-// create a new observer, passing in the callback function
 var observer = new MutationObserver(callback);
-// start observing the targetNode with the given configuration
+
 observer.observe(targetNode, config);
 
-// arranges fields
 function arrangeFields(){
-  // stores parent node
   var parentNode = document.getElementsByClassName("block-outer-scroll form-content")[0];
-
-  // stores child nodes
   var emailField = document.getElementsByClassName("email field-container")[0];
   var customField = document.getElementsByClassName("custom-fields field-container")[0];
   var captchaField = document.getElementsByClassName("captcha field-container")[0];
@@ -23,8 +15,6 @@ function arrangeFields(){
   var issueField = document.getElementsByClassName("issue field-container")[0];
   var attachField = document.getElementsByClassName("attachments-list")[0];
   var subjField = document.getElementsByClassName("subject field-container")[0];
-
-  //appends child nodes to parent node in desired order
 
   parentNode.append(emailField)
   parentNode.append(subjField)
